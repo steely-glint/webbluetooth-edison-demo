9ac016a33b4445fdd88cdfdf2e0cbbbd74bc8fb3
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2016-03-22 14:04:49 +0100

    Create Gulp tasks for generating push and cache manifest

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -25,41 +25,82 @@ var dist = function(subpath) {
   return !subpath ? DIST : path.join(DIST, subpath);
 };
 
-gulp.task('cache-config', function(callback) {
+var ensurePath = function(file) {
+  // Make a path if one wasn't given. e.g. basic.html -> ./basic.html
+  return (file.indexOf(path.sep) === -1) ? `.${path.sep}${file}` : file;
+};
+
+gulp.task('push-manifest', function(done) {
+  let files = ['index.html'];
+  let result = {};
+  let manifestName = 'push_manifest.json';
+
+  let singleFile = files.length < 2;
+
+  function processFile(file) {
+    let f = ensurePath(file);
+    let basePath = f.slice(0, f.lastIndexOf(path.sep))
+    let inputPath = f.slice(f.lastIndexOf(path.sep) + 1);
+
+    let manifest = new Manifest({basePath, inputPath, name: manifestName });
+    return manifest.generate().then(output => {
+      if (singleFile)
+        manifest.write(output.file);
+      else
+        result[inputPath] = output.file;
+    });
+  }
+
+  return Promise.all(files.map(file => processFile(file)))
+  .then(() => {
+    if (!singleFile) {
+      let manifest = new Manifest({name: manifestName})
+      manifest.write(result);
+    }
+  });
+});
+
+gulp.task('cache-config', function(done) {
   var dir = dist();
   var config = {
     cacheId: packageJson.name || path.basename(__dirname),
     disabled: false
   };
 
-  let f = 'index.html';
+  let files = ['index.html'];
+  let manifestName = 'cache-config.json';
 
-  // Make a path if one wasn't given. e.g. basic.html -> ./basic.html
-  if (f.indexOf(path.sep) === -1) {
-    f = `.${path.sep}${f}`;
+  function processFile(file) {
+    let f = ensurePath(file);
+    let basePath = f.slice(0, f.lastIndexOf(path.sep))
+    let inputPath = f.slice(f.lastIndexOf(path.sep) + 1);
+
+    let manifest = new Manifest({basePath, inputPath, name: manifestName });
+    return manifest.generate().then(output => {
+      config.precache = output.urls.map((url) => {
+        return url.startsWith('/') ? url.slice(1) : url;
+      });
+    });
   }
 
-  let basePath = f.slice(0, f.lastIndexOf(path.sep))
-  let inputPath = f.slice(f.lastIndexOf(path.sep) + 1);
-
-  let manifest = new Manifest({basePath, inputPath, name: 'cache-config.json'});
-  manifest.generate().then(output => {
-    config.precache = output.urls.map((url) => {
-      return url.startsWith('/') ? url.slice(1) : url;
-    });
+  return Promise.all(files.map(file => processFile(file)))
+  .then(() => {
     config.precache.push('./');
 
     let md5 = crypto.createHash('md5');
     md5.update(JSON.stringify(config.precache));
     config.precacheFingerprint = md5.digest('hex');
 
-    var configPath = path.join(dir, 'cache-config.json');
-    fs.writeFile(configPath, JSON.stringify(config), callback);
-  })
+    var configPath = path.join(dir, manifestName);
+    return new Promise(function(resolve) {
+      fs.writeFile(configPath, JSON.stringify(config), resolve);
+    });
+  });
 });
 
 gulp.task('default', function(cb) {
   runSequence(
     'cache-config',
+    'push-manifest',
     cb);
 });
