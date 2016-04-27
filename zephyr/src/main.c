692e5d59b80fc9cb0ed8c83cec72d1200fa08a2a
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2016-04-27 15:13:25 +0200

    Add Zephyr ADC code path to read from TMP36

--- zephyr/src/main.c
+++ zephyr/src/main.c
@@ -2,6 +2,8 @@
 #include <device.h>
 #include <pwm.h>
 #include <sys_clock.h>
+#include <misc/byteorder.h>
+#include <adc.h>
 
 #if defined(CONFIG_STDOUT_CONSOLE)
 #include <stdio.h>
@@ -15,16 +17,42 @@
 #define IO5_GREEN "PWM_1"
 #define IO6_BLUE  "PWM_2"
 
+#define ADC_DEVICE_NAME "ADC_0"
+
+/*
+ * The analog input pin and channel number mapping
+ * for Arduino 101 board.
+ * A0 Channel 10
+ * A1 Channel 11
+ * A2 Channel 12
+ * A3 Channel 13
+ * A4 Channel 14
+ */
+#define A0 10
+#define BUFFER_SIZE 40
+
+static uint8_t buffer[BUFFER_SIZE];
+
+static struct adc_seq_entry sample = {
+	.sampling_delay = 12,
+	.channel_id = A0,
+	.buffer = buffer,
+	.buffer_length = BUFFER_SIZE,
+};
+
+static struct adc_seq_table table = {
+	.entries = &sample,
+	.num_entries = 1,
+};
+
 #define SLEEPTICKS  SECONDS(1)
 
 void main(void)
 {
   struct nano_timer timer;
-  void *timer_data[1];
+	uint32_t data[2] = {0, 0};
 
-  struct device *red, *green, *blue;
-
-  nano_timer_init(&timer, timer_data);
+  struct device *red, *green, *blue, *tmp36;
 
   PRINT("Zephyr WebBluetooth demo\n");
 
@@ -33,13 +61,34 @@ void main(void)
   blue = device_get_binding(IO6_BLUE);
 
   if (!red || !green || !blue) {
-    PRINT("Cannot find LED connected to pin 3 (red), 5 (green) and 6 (blue)!\n");
+    PRINT("Cannot find LED connected to pin 3 (red), 5 (green) and 6 (blue).\n");
   }
+
+  tmp36 = device_get_binding(ADC_DEVICE_NAME);
+  if (!tmp36) {
+    PRINT("Cannot find the TMP36 connected to pin A0.\n");
+  }
+
+	nano_timer_init(&timer, data);
+	adc_enable(tmp36);
 
   while (1) {
     pwm_pin_set_values(red, 1024, SLEEPTICKS, 0);
 
+    if (adc_read(tmp36, &table) == 0) {
+      uint32_t length = BUFFER_SIZE;
+      uint8_t *buf = buffer;
+      for (; length > 0; length -= 4, buf += 4) {
+		    uint32_t rawValue = *((uint32_t *) buf);
+        float voltage = (rawValue / 1024.0) * 5.0;
+        float celsius = (voltage - 0.5) * 100;
+        PRINT("Celsius %f\n", celsius);
+	    }
+    }
+
     nano_timer_start(&timer, SLEEPTICKS);
     nano_timer_test(&timer, TICKS_UNLIMITED);
   }
+
+  adc_disable(tmp36);
 }
