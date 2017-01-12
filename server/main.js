5a3ab0d403168aac7ac0f9c6c2e69f1c6d195286
T H Panton <thp@westhawk.co.uk>
2017-01-12 10:14:38 +0000

    not -yet- working transfer of 3 data points.

--- server/main.js
+++ server/main.js
@@ -2,45 +2,30 @@ var beacon = require("eddystone-beacon");
 var util = require('util');
 var bleno = require('bleno');
 
-DEVICE_NAME = 'Edison';
+DEVICE_NAME = 'Pipe';
 
 process.env['BLENO_DEVICE_NAME'] = DEVICE_NAME;
 
-var FingerprintCharacteristic = function() {
+var ByteCharacteristic = function(uuid,val) {
   bleno.Characteristic.call(this, {
-    uuid: 'fc0a',
+    uuid: uuid,
     properties: ['read'],
     value: null
   });
-  this._value = new Buffer([0x89,0x88,0x07,0x3b,0x0c,0x73,0x02,0x5c,0x23,0x26,0xbb,0xbe,0x06,0xcd,0x71,0xfc,0x9f,0x3c,0xe3,0x5c,0x68,0xc5,0x25,0xec,0x28,0x57,0x22,0x1b,0x19,0x83,0x8f]);
+  this._value = val;
 
 };
 
-util.inherits(FingerprintCharacteristic, bleno.Characteristic);
+util.inherits(ByteCharacteristic, bleno.Characteristic);
 
-FingerprintCharacteristic.prototype.onReadRequest = function(offset, callback) {
-  console.log('reading fingerprint');
+ByteCharacteristic.prototype.onReadRequest = function(offset, callback) {
+  console.log('reading uuid '+this.uuid);
 
   callback(this.RESULT_SUCCESS, this._value);
 };
 
 
-var NonceCharacteristic = function() {
-  bleno.Characteristic.call(this, {
-    uuid: 'fc0b',
-    properties: ['read'],
-    value: null
-  });
-  this._value = new Buffer([0xff,0xfe,0xfd,0xfb,0xfa,0xf9,0xf8,0xf7,0xf6,0xf5,0xf4,0xf3,0xf2,0xf1,0xf0]);
-};
 
-util.inherits(NonceCharacteristic, bleno.Characteristic);
-
-NonceCharacteristic.prototype.onReadRequest = function(offset, callback) {
-  console.log('reading Nonce');
-
-  callback(this.RESULT_SUCCESS, this._value);
-};
 
 console.log("waiting for bleno");
 
@@ -48,7 +33,7 @@ bleno.on('stateChange', function(state) {
   console.log(state);
 
   if (state === 'poweredOn') {
-    bleno.startAdvertising(DEVICE_NAME, ['fc00']);
+    bleno.startAdvertising(DEVICE_NAME, ['919e']);
     beacon.advertiseUrl("https://pi.pe/iot/bl.html", {name: DEVICE_NAME});
   } else {
     if (state === 'unsupported'){
@@ -58,8 +43,9 @@ bleno.on('stateChange', function(state) {
   }
 });
 
-var fingerprintCharacteristic = new FingerprintCharacteristic();
-var nonceCharacteristic = new NonceCharacteristic();
+var fingerprintACharacteristic = new ByteCharacteristic('fc0a',new Buffer([0xBD,0xE3,0xCC,0x6C,0xA1,0x7A,0x60,0xB0,0x3C,0x43,0x37,0x36,0x69,0xEE,0x7C,0xA6]));
+var fingerprintBCharacteristic = new ByteCharacteristic('fc0b',new Buffer([0x5D,0x67,0xFE,0xFF,0x72,0x92,0x66,0x49,0x4C,0x13,0x11,0x8C,0xEC,0xE2,0xC2,0xA3]));
+var nonceCharacteristic = new ByteCharacteristic('fc0c',new Buffer([0xff,0xfe,0xfd,0xfb,0xfa,0xf9,0xf8,0xf7,0xf6,0xf5,0xf4,0xf3,0xf2,0xf1,0xf0]));
 
 
 bleno.on('advertisingStart', function(error) {
@@ -72,9 +58,9 @@ bleno.on('advertisingStart', function(error) {
 
   bleno.setServices([
     new bleno.PrimaryService({
-      uuid: 'fc00',
+      uuid: '919e',
       characteristics: [
-        fingerprintCharacteristic,
+        fingerprintACharacteristic,fingerprintBCharacteristic,
         nonceCharacteristic
       ]
     })
